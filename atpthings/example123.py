ba70a8fb9ec13e7a772f074ff43a2b4e65dc7401
andrazpolak <andrazpolak@gmail.com>
2022-01-19 18:25:32 +0100

    tmp

---
+++ atpthings/example123.py
@@ -0,0 +1,9 @@
+def add_two(number: int) -> int:
+    """[summary]
+
+    :param number: [description]
+    :type number: int
+    :return: [description]
+    :rtype: int
+    """
+    return number + 2
---
+++ atpthings/util/dataframe.py
@@ -0,0 +1,9 @@
+"""ATPthings: Pandas.DataFrame Utils
+
+"""
+
+
+def add_one(number: int) -> int:
+    """ATPthings: Pandas.DataFrame Utils"""
+
+    return number + 1
